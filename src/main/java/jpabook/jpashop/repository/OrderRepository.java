160bdf6f18da51a4c340e54c56db0983149b25d0
limwoobin <drogba02@naver.com>
2021-05-30 16:56:05 +0900

    add - order 동적쿼리 테스트

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,8 +20,13 @@ public class OrderRepository {
         return em.find(Order.class , id);
     }
 
-    public List<Order> findAll() {
-        return em.createQuery("select o from Order o")
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name" , Order.class)
+                .setParameter("status" , orderSearch.getOrderStatus())
+                .setParameter("name" , orderSearch.getMemberName())
+                .setMaxResults(1000)
                 .getResultList();
     }
 }
