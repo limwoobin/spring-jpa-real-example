cd38be43b28d7b580ef0d7946e6b24a358f90447
limwoobin <drogba02@naver.com>
2021-05-30 16:42:45 +0900

    add - order test code 작성

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,8 +1,13 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String city;
     private String street;
