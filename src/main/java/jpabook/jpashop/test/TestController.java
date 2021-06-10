787ba700bc5c0fb3f0fbc164e703ecb260317c99
limwoobin <drogba02@naver.com>
2021-06-10 15:33:26 +0900

    @Transactional(readOnly = true) , @Transactional 없는 경우 두개의 차이
    - osiv 옵션 키고 테스트 하고 끄고 테스트하고 진행

--- src/main/java/jpabook/jpashop/test/TestController.java
+++ src/main/java/jpabook/jpashop/test/TestController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.test;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,16 +15,25 @@ public class TestController {
     private final TestService testService;
 
     // Lazy 에서 하나만 조회하는 경우는 N + 1 발생 안한다
-    @GetMapping("/test/{id}")
+    @GetMapping("/v1/test/{id}")
     public TestDTO test(@PathVariable Long id) {
         Test test = testService.test(id);
-        System.out.println(test.getClass());
-        System.out.println(test.getOthers().getClass());
-        test.getOthers();
+//        for (Other other : test.getOthers()) {
+//            other.getOtherName();
+//        }
 
         return null;
     }
 
+    @GetMapping("/v2/test/{id}")
+    public TestDTO test2(@PathVariable Long id) {
+        Test test = testService.test2(id);
+//        for (Other other : test.getOthers()) {
+//            other.getOtherName();
+//        }
+
+        return null;
+    }
 
     @GetMapping("/test2/{id}")
     public TestDTO findOne(@PathVariable Long id) {
@@ -36,9 +46,19 @@ public class TestController {
     }
 
     // N + 1
-    @GetMapping("/test")
+    @GetMapping("/v1/test")
     public List<TestDTO> test() {
         List<Test> tests = testService.findAll();
+        for (Test test : tests) {
+            System.out.println(test.getOthers().size());
+        }
+
+        return null;
+    }
+
+    @GetMapping("/v2/test")
+    public List<TestDTO> test2() {
+        List<Test> tests = testService.findAll2();
         for (Test test : tests) {
             System.out.println(test.getOthers().size());
         }
--- src/main/java/jpabook/jpashop/test/TestService.java
+++ src/main/java/jpabook/jpashop/test/TestService.java
@@ -13,11 +13,27 @@ public class TestService {
 
     @Transactional(readOnly = true)
     public Test test(Long id) {
-        return testRepository.findTest(id);
+        Test test = testRepository.findTest(id);
+        for (Other other : test.getOthers()) {
+            other.getOtherName();
+        }
+        return test;
+    }
+
+    public Test test2(Long id) {
+        Test test = testRepository.findTest(id);
+        for (Other other : test.getOthers()) {
+            other.getOtherName();
+        }
+        return test;
     }
 
     @Transactional(readOnly = true)
     public List<Test> findAll() {
+        return testRepository.findAll();
+    }
+
+    public List<Test> findAll2() {
         return testRepository.findAll();
     }
 
